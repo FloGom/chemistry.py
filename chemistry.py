5cf14316767632b0af420f7951a8f7adffc2efa4
PythonNut <PythonNut@users.noreply.github.com>
2014-09-16 21:35:15 +0000

    Generate elements from BODR

--- chemistry.py
+++ chemistry.py
@@ -38,3 +38,42 @@ class Element(object):
     __str__ = __repr__
     
     
+import xml.etree.cElementTree as ET
+
+tree = ET.ElementTree(file="bodr-10/elements/elements.xml")
+root = tree.getroot()
+
+keymap = {
+    "bo:atomicNumber"             : ("number",            "text"),
+    "bo:symbol"                   : ("symbol",            "value"),
+    "bo:name"                     : ("name",              "value"),
+    "bo:mass"                     : ("mass",              "text"),
+    "bo:exactMass"                : ("exact_mass",        "text"),
+    "bo:radiusCovalent"           : ("radius_covalent",   "text"),
+    "bo:radiusVDW"                : ("radius_vdw",        "text"),
+    "bo:ionization"               : ("ionization",        "text"),
+    "bo:electronAffinity"         : ("electron_affinity", "text"),
+    "bo:electronegativityPauling" : ("electronegativity", "text"),
+    "bo:boilingpoint"             : ("boiling_point",     "text"),
+    "bo:meltingpoint"             : ("melting_point",     "text"),
+    "bo:group"                    : ("group",             "text"),
+    "bo:periodTableBlock"         : ("block",             "text"),
+    "bo:period"                   : ("period",            "text"),
+    "bo:family"                   : ("family",            "text"),
+    "bo:electronicConfiguration"  : ("electron_config",   "text"),
+}
+
+for element in root[2:]:
+    properties = {}
+    for property in element:
+        key = property.attrib["dictRef"]
+        if key not in keymap:
+            continue
+        key = keymap[key]
+        if key[1] == "text":
+            properties[key[0]] = property.text
+        else:
+            properties[key[0]] = property.attrib[key[1]]
+    
+    globals()[properties["symbol"]] = Element(properties)
+
